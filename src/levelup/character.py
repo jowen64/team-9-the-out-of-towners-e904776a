a4fcbcc09400c4b14f38e29621bd819fd2b349c7
Mathew <loeschms@gmail.com>
2023-11-16 17:23:40 -0500

    Character cleanup

--- src/levelup/character.py
+++ src/levelup/character.py
@@ -4,12 +4,10 @@ from levelup.map import Map
 
 DEFAULT_CHARACTER_NAME = "Character"
 class Character:
-    # In python, we don't use getters. So no getPosition or getName for this class
     name = ""
     current_position :Position = Position(-100,-100)
     map :Map = Map()
 
-    # Since python doesn't do method overloading, this is how we support a constructor with optional parameters
     def __init__(self, character_name=DEFAULT_CHARACTER_NAME):
         if character_name.strip() == '':
             self.name = DEFAULT_CHARACTER_NAME
@@ -19,10 +17,7 @@ class Character:
     def move(self, direction :Direction) -> None:
         self.current_position = self.map.calculate_new_position(
             self.current_position, direction)
-        
 
     def enter_map(self, map :Map) -> None:
         self.map = map
         self.current_position = self.map.starting_position
-        # TODO: implement method here and remove the print statement below
-        # print("enter_map method not yet implemented")
