3dd92a6e590a95cfa52223ecebb2e05a14eb9136
Jon Owen <jowen64@gmail.com>
2023-11-16 16:02:44 -0500

    JO UI update

--- src/levelup/ui.py
+++ src/levelup/ui.py
@@ -9,7 +9,7 @@ VALID_COMMANDS = VALID_DIRECTIONS + ['q']
 class GameApp:
 
     controller: GameController
-    starting_pos = (-100,-100)
+    starting_pos = (0,0)
 
     def __init__(self):
         self.controller = GameController()
@@ -26,7 +26,7 @@ class GameApp:
     def create_character(self):
         character = self.prompt("Welcome to the Out-of-Towners\n Enter character name", lambda x: len(x) > 0)
         self.controller.create_character(character)
-        print(f"Welcome, {self.controller.status.character_name}")
+        print(f"Welcome, {self.controller.status.character_name} You are starting on {self.starting_pos}")
 
     def move_loop(self):
         while True:
